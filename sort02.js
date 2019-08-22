17043eb6e5b7d756c500e16196e9885cc86fa66e
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-08-22 12:37:38 +0900

    sort

--- sort02.js
+++ sort02.js
@@ -1,11 +1,29 @@
 const test = () => {
-  let answer = [3, 30, 34, 5, 9];
-  //   answer.sort();
-  //   answer.reverse();
+  let answer = [0, 0, 0, 1];
   let pd = answer
-    .map(v => v+'')
+    .map(v => v + "")
     .sort((a, b) => (b + a) * 1 - (a + b) * 1)
-    .join('')
-  return answer[0] === "0" ? "0" : answer;
+    .join("");
+  console.log(pd[0] === "0" ? "0" : pd);
+  return;
 };
 test();
+
+function solution(numbers) {
+  var answer = "";
+  // 1. 숫자를 문자열로 변환
+  // numbers.map(num => {
+  //   num.toString();
+  // });
+  numbers.sort((a, b) => {
+    if (a === b) {
+      return false;
+    }
+    console.log(a, b);
+    let aText = a.toString();
+    let bText = b.toString();
+    return aText + bText > bText + aText ? -1 : 1;
+  });
+  console.log(numbers.join(""));  
+}
+solution([0,0,0,1])
--- sort03.js
+++ sort03.js
@@ -1,5 +1,5 @@
 const test = () => {
-  let answer = [4,0,1,7,2,0] ;
+  let answer = [5, 0, 0];
   let j = 0;
   let result = [];
   answer.sort((a, b) => a - b);
@@ -11,3 +11,24 @@ const test = () => {
   console.log(Math.max(...result));
 };
 test();
+function solution(arr) {
+  var answer = 0;
+  arr.sort((a, b) => a - b);
+  let h = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= arr.length - i) {
+      h.push(arr[i]);
+      console.log(h);
+    } else {
+      h.push(arr.length - i);
+    }
+  }
+  if (h.length === 0 || Math.min(...arr) >= arr.length) {
+    return arr.length;
+  }
+  console.log(h);
+  console.log(Math.max(...h));
+  return Math.max(...h);
+  //  return h[h.length-1];
+}
+solution([5, 0, 0]);
