73079259200807c9ed480b8ec44b6683180cf1c6
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-08-28 12:13:44 +0900

    refactoring

--- greedy02.js
+++ greedy02.js
@@ -25,7 +25,7 @@ const test = () => {
   let O = 12;
   let M = 12;
   let N = 13;
-  let name = "JAAAAAAAJN";
+  let name = "JJAAAAAAAJN";
   let sum = 0;
   let Aarr = [];
   let length, reverseLength;
@@ -35,22 +35,111 @@ const test = () => {
       Aarr.push(i);
     }
   }
-  for (let i=1; i < name.length; i++) {
-    if (name[i] !== 'A') {
-        reverseLength = name.length - i;
-        break;
+  for (let i = 1; i < name.length; i++) {
+    if (name[i] !== "A") {
+      reverseLength = name.length - i;
+      break;
     }
-}
+  }
 
-for (let i=name.length; i > 0; i--) {
-    if (name[i] !== 'A') {
-        length = i;
-        break;
+  for (let i = name.length; i > 0; i--) {
+    if (name[i] !== "A") {
+      length = i;
+      break;
     }
-}
-return sum + (length < reverseLength ? length : reverseLength);
-console.log(length, reverseLength);
-  console.log(Aarr)
+  }
+  return sum + (length < reverseLength ? length : reverseLength);
+  console.log(length, reverseLength);
+  console.log(Aarr);
   console.log(sum + name.length - 1);
 };
-test();
+
+function find_Direction(name, cursor, totalMoved, isVisited) {
+  let rightDis = 0;
+  let leftDis = 0;
+  let nextCursor = 0;
+  let total = totalMoved;
+  isVisited[cursor] = true;
+
+  for (let i = 1; i < name.length; i++) {
+    let newCursor =
+      cursor + i > name.length - 1 ? (cursor + i) % name.length : cursor + i;
+
+    if (isVisited[newCursor] == false) {
+      if (name[newCursor] !== "A") {
+        rightDis = i;
+        break;
+      }
+    }
+  }
+
+  for (let i = 1; i < name.length; i++) {
+    let newCursor = cursor - i < 0 ? cursor - i + name.length : cursor - i;
+    if (isVisited[newCursor] == false) {
+      if (name[newCursor] !== "A") {
+        leftDis = i;
+        break;
+      }
+    }
+  }
+  if (rightDis == 0 && leftDis == 0) {
+    return total;
+  }
+  if (rightDis > leftDis) {
+    total += leftDis;
+    nextCursor =
+      cursor - leftDis < 0 ? cursor - leftDis + name.length : cursor - leftDis;
+  } else {
+    // 같거나 오른쪽으로 이동해야할 때,
+    total += rightDis;
+    nextCursor = cursor + rightDis;
+  }
+
+  return find_Direction(name, nextCursor, total, isVisited);
+}
+
+function solution(name) {
+  var answer = 0;
+  let alphabet = {
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+    H: 7,
+    I: 8,
+    J: 9,
+    K: 10,
+    L: 11,
+    M: 12,
+    N: 13,
+    O: 12,
+    P: 11,
+    Q: 10,
+    R: 9,
+    S: 8,
+    T: 7,
+    U: 6,
+    V: 5,
+    W: 4,
+    X: 3,
+    Y: 2,
+    Z: 1
+  };
+
+  for (let i of name) {
+    answer += alphabet[i];
+  }
+  let isVisited = [];
+  isVisited.length = name.length;
+  isVisited.fill(false);
+
+  let cursor = 0;
+  let moved = find_Direction(name, cursor, 0, isVisited);
+
+  return answer + moved;
+}
+
+console.log(solution("XXAAAAAAAAAAAAAXX"));
--- greedy03.js
+++ greedy03.js
@@ -1,28 +1,67 @@
-const test = () => {
+// const test = () => {
+//   let number = 4177252841;
+//   let str = String(number);
+//   let k = 4;
+//   let arr = [];
+//   let arr1 = [];
+//   let answer = "";
+//   for (let i = 0; i < str.length; i++) {
+//     arr.push(Number(str[i]));
+//     arr1.push(Number(str[i]));
+//   }
+//   arr1.sort((a, b) => {
+//     return a < b ? -1 : a > b ? 1 : 0;
+//   });
+//   console.log(arr1);
+//   console.log(arr);
+//   for (let i = 0; i < k; i++) {
+//     arr.splice(arr.indexOf(arr1[i]), 1);
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     answer += arr[i] + "";
+//   }
+//   console.log(arr1);
+//   console.log(arr);
+//   console.log(answer);
+//   return answer;
+// };
+const test1 = () => {
   let number = 4177252841;
-  let str = String(number);
   let k = 4;
-  let arr = [];
   let arr1 = [];
-  let answer = "";
-  for (let i = 0; i < str.length; i++) {
-    arr.push(Number(str[i]));
-    arr1.push(Number(str[i]));
+  let numberarr = [];
+  let arr = [];
+  let strnumber = String(number);
+  for (let i = 0; i < strnumber.length; i++) {
+    numberarr.push(Number(strnumber[i]));
   }
-  arr1.sort((a, b) => {
-    return a < b ? -1 : a > b ? 1 : 0;
-  });
-  console.log(arr1)
-  console.log(arr)
-  for (let i = 0; i < k; i++) {
-    arr.splice(arr.indexOf(arr1[i]), 1);
+  while (k > 0) {
+    console.log(k);
+    console.log(numberarr);
+    for (let i = 0; i < k; i++) {
+      arr.push(Number(numberarr[i]));
+    }
+    console.log(arr);
+    let max = arr.indexOf(arr.reduce(function(a, b) {
+      return Math.max(a, b);
+  }));
+    k -= max;
+    for (let i = 0; i < k; i++) {
+      numberarr.shift();
+    }
+    arr1.push(numberarr.shift());
+    arr=[]
+    console.log(arr);
+    console.log(arr1, numberarr);
   }
-  for(let i =0; i<arr.length; i++){
-    answer+=arr[i]+""
-  }
-  console.log(arr1)
-  console.log(arr)
-  console.log(answer)
-  return answer;
+  // console.log(max);
+  // for (let i = max; i < strnumber.length; i++) {
+  //   arr1.push(Number(strnumber[i]));
+  // }
+  // console.log(arr1);
+  // for (let i = 0; i < k - max; i++) {
+  //   arr1.splice(arr1.indexOf(Math.min.apply(null, arr1)), 1);
+  // }
+  // console.log(arr1);
 };
-test();
+test1();
