a94f78dfac5a01880b40dc99e038b0f71a7d9274
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-10-10 19:08:22 +0900

    Update DFS_BFS01.js

--- DFS_BFS01.js
+++ DFS_BFS01.js
@@ -1,29 +1,24 @@
 const test = () => {
-    let numbers = [1, 1, 1, 1, 1];
-    let target = 3;
-    let answer = 0;
-    const solution = (numbers, target) => {
-        dfs(target, numbers, 0)
-        return answer;
+  let numbers = [1, 1, 1, 1, 1];
+  let target = 3;
+  let answer = 0;
+  const dfs = (target, numbers, k) => {
+    if (k === numbers.length) {
+      let sum = 0;
+      for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+      }
+      if (sum === target) answer++;
+      return;
+    } else {
+      numbers[k] *= 1;
+      dfs(target, numbers, k + 1);
+      numbers[k] *= -1;
+      dfs(target, numbers, k + 1);
     }
-
-    const dfs = (target, numbers, k) => {
-        if (k === numbers.length) {
-            let sum = 0;
-            for (let i = 0; i < numbers.length; i++) {
-                sum += numbers[i];
-            }
-            if (sum === target)
-                answer++
-            return;
-        }
-        else {
-            numbers[k] *= 1;
-            dfs(target, numbers, k + 1);
-            numbers[k] *= -1;
-            dfs(target, numbers, k + 1);
-        }
-    }
-    solution(numbers, target)
-}
-test()
+  };
+  dfs(target, numbers, 0);
+  console.log(answer)
+  return answer;
+};
+test();
