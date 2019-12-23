c177b29f19cbcc9aef22ca617b2947decb6520f2
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-12-23 16:07:41 +0900

    Update summerCoding01.js

--- summerCoding01.js
+++ summerCoding01.js
@@ -1,31 +1,42 @@
 const test = () => {
-    const words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "asd"];
-    const n = 3;
-    const array = Array(n).fill(null).map(() => Array());
-    for (let i = 0; i < words.length; i++) {
-        if (words.indexOf(words[i]) !== i) {
-            const p = Math.ceil((i + 1) / n)
-            let k = ((i + 1) - (Math.floor((i + 1) / n) * n));
-            if (k === 0) {
-                k = n
-            }
-            return ([k, p])
-        }
+  const words = [
+    "tank",
+    "kick",
+    "know",
+    "wheel",
+    "land",
+    "dream",
+    "mother",
+    "robot",
+    "asd"
+  ];
+  const n = 3;
+  const array = Array(n)
+    .fill(null)
+    .map(() => Array());
+  for (let i = 0; i < words.length; i++) {
+    if (words.indexOf(words[i]) !== i) {
+      const p = Math.ceil((i + 1) / n);
+      let k = i + 1 - Math.floor((i + 1) / n) * n;
+      if (k === 0) {
+        k = n;
+      }
+      return [k, p];
     }
-    let count = 1;
-    array[0].push(words[0])
-    for (let i = 1; i < words.length; i++) {
-        if (words[i - 1][words[i - 1].length - 1] === words[i][0]) {
-            array[count].push(words[i])
-        }
-        else {
-            return (count + 1, Math.ceil((i + 1) / n))
-        }
-        count++;
-        if (count === n) {
-            count = 0
-        }
+  }
+  let count = 1;
+  array[0].push(words[0]);
+  for (let i = 1; i < words.length; i++) {
+    if (words[i - 1][words[i - 1].length - 1] === words[i][0]) {
+      array[count].push(words[i]);
+    } else {
+      return count + 1, Math.ceil((i + 1) / n);
     }
-    return [0, 0]
-}
-test()
+    count++;
+    if (count === n) {
+      count = 0;
+    }
+  }
+  return [0, 0];
+};
+test();
